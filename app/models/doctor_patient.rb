426ea9ccf3a92928fbac160db60d03a5d5877121
Leo Banos Garcia <bsleo625@gmail.com>
2023-01-13 10:47:23 -0700

    Adds find by doctor and patient id method for doctorpatient

--- app/models/doctor_patient.rb
+++ app/models/doctor_patient.rb
@@ -1,4 +1,8 @@
 class DoctorPatient < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
+
+  def self.find_by_doctor_and_patient_id(doctor, patient)
+    DoctorPatient.where(doctor_id:doctor,patient_id:patient).first
+  end
 end
--- spec/models/doctor_patient_spec.rb
+++ spec/models/doctor_patient_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe DoctorPatient, type: :model do
     it {should belong_to :doctor}
     it {should belong_to :patient}
   end
+
+  describe "class methods" do
+    before :each do
+      load_test_data
+    end
+    it 'can find a doctor patient from a doctors id and patient id combo' do
+      expect(DoctorPatient.find_by_doctor_and_patient_id(@doctor_1.id, @patient_1.id)).to eq(@doctor_patient_1)
+      expect(DoctorPatient.find_by_doctor_and_patient_id(@doctor_3.id, @patient_4.id)).to eq(@doctor_patient_7)
+      expect(DoctorPatient.find_by_doctor_and_patient_id(@doctor_4.id, @patient_1.id)).to eq(@doctor_patient_9)
+    end
+  end
 end
