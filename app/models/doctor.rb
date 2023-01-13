de0d4cea02e50c4ec6d0ba9832ee37a83a3a285e
Leo Banos Garcia <bsleo625@gmail.com>
2023-01-13 09:25:37 -0700

    adds validations

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,4 +2,8 @@ class Doctor < ApplicationRecord
   belongs_to :hospital
   has_many :doctor_patients
   has_many :patients, through: :doctor_patients
+
+  validates_presence_of :name
+  validates_presence_of :specialty
+  validates_presence_of :university
 end
