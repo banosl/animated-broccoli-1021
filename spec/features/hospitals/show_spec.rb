8bed2884fa499961c029e50dd5c2e4164f3bbac6
Leo Banos Garcia <bsleo625@gmail.com>
2023-01-13 11:46:44 -0700

    Adds the number of patients to the next of each doctor

--- spec/features/hospitals/show_spec.rb
+++ spec/features/hospitals/show_spec.rb
@@ -11,9 +11,17 @@ RSpec.describe "Hospital Show Page", type: :feature do
       expect(page).to have_content("#{@hospital_1.name}")
       expect(page).to have_content("#{@doctor_1.name}")
       expect(page).to have_content("#{@doctor_2.name}")
+      expect(page).to_not have_content("#{@doctor_3.name}")
+      expect(page).to_not have_content("#{@doctor_5.name}")
     end
 
-    it "next to each doctor is the number of patients associated with them"
+    it "next to each doctor is the number of patients associated with them" do
+      visit hospital_path(@hospital_2.id)
+
+      expect(page).to have_content("#{@doctor_3.name}, Patients: #{@doctor_3.patient_count}")
+      expect(page).to have_content("#{@doctor_4.name}, Patients: #{@doctor_4.patient_count}")
+      expect(page).to have_content("#{@doctor_5.name}, Patients: #{@doctor_5.patient_count}")
+    end
 
     it "list of doctors is organized by most number of patients to least number of patients"
   end
